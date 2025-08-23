4e102fcf509666c303e01ee4d1248c14a562de61
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-08-23 17:25:50 +0530

    Add polar mode

--- src/LineItem.cpp
+++ src/LineItem.cpp
@@ -1,7 +1,25 @@
 #include "LineItem.hpp"
 
+#include <print>
+
 void
-LineItem::render() noexcept
+LineItem::setPolarMode(bool state) noexcept
 {
-    DrawRectangle(m_pos.x, m_pos.y, m_width, m_height, m_color);
+    m_polarMode = state;
+    if (state)
+    {
+        m_renderFunction = [this]()
+        {
+            Vector2 center = { (float)m_pos.x, (float)m_pos.y };
+            Vector2 size   = { (float)m_width, (float)m_height };
+            DrawRectanglePro({ center.x - size.x / 2, center.y - size.y / 2,
+                               size.x, size.y },
+                             { size.x, size.y }, m_angle, m_color);
+        };
+    }
+    else
+    {
+        m_renderFunction = [this]()
+        { DrawRectangle(m_pos.x, m_pos.y, m_width, m_height, m_color); };
+    }
 }
--- src/LineItem.hpp
+++ src/LineItem.hpp
@@ -2,6 +2,7 @@
 
 #include "DVector2.hpp"
 
+#include <functional>
 #include <raylib.h>
 
 class LineItem
@@ -11,12 +12,24 @@ private:
     Color m_color{ RED };
     DVector2<int> m_pos;
     int m_width, m_height;
+    bool m_polarMode{ false };
+    float m_angle{ 0.0f };
+
+    using RenderFunction = std::function<void()>;
+    RenderFunction m_renderFunction;
 
 public:
 
+    void setPolarMode(bool state) noexcept;
+
+    inline void setAngle(float angle) noexcept { m_angle = angle; }
+    inline bool polarMode() const noexcept { return m_polarMode; }
     inline void setPos(DVector2<int> pos) noexcept { m_pos = pos; }
     inline void setWidth(int w) noexcept { m_width = w; }
     inline void setHeight(int h) noexcept { m_height = h; }
-    void render() noexcept;
     inline auto pos() const -> decltype(m_pos) { return m_pos; }
+    inline void render() const
+    {
+        if (m_renderFunction) m_renderFunction();
+    }
 };
