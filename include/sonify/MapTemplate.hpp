200d7717e61474c04226dfbd1d51cd08cf261026
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-08-23 23:03:24 +0530

    Add MapTemplate

---
+++ include/sonify/MapTemplate.hpp
@@ -0,0 +1,34 @@
+// File for creating custom mappings using shared objects
+#pragma once
+
+#include "Pixel.hpp"
+#include "utils.hpp"
+
+#include <vector>
+
+class MapTemplate
+{
+public:
+
+    using FreqMapFunc = short (*)(double, double, double, double, double);
+
+    virtual ~MapTemplate()                                         = default;
+    virtual const char *name() const                               = 0;
+    virtual std::vector<short> mapping(const std::vector<Pixel> &) = 0;
+
+    inline void setMinFreq(int f) noexcept { _min_freq = f; }
+    inline void setMaxFreq(int f) noexcept { _max_freq = f; }
+    inline void setSampleRate(int f) noexcept { _sample_rate = f; }
+    inline void setFreqMap(FreqMapFunc f) noexcept { freq_map = f; }
+
+protected:
+
+    FreqMapFunc freq_map{ utils::LinearMap };
+    int _min_freq{ 0 }, _max_freq{ 20000 }, _sample_rate{ 44100 };
+};
+
+// Factory function for plugins
+extern "C"
+{
+    MapTemplate *create();
+}
