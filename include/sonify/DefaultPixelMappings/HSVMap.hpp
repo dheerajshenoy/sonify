2078452f649e181556bc2b32cf18a2248fa7a116
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-08-26 23:49:23 +0530

    Add HSVMap default pixel mapping

---
+++ include/sonify/DefaultPixelMappings/HSVMap.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "sonify/MapTemplate.hpp"
+
+class HSVMap : public MapTemplate
+{
+public:
+
+    const char *name() const override { return "HSV"; }
+
+    std::vector<short>
+    mapping(const std::vector<Pixel> &pixelCol) noexcept override
+    {
+        std::vector<short> fs;
+        int N = static_cast<int>(pixelCol.size());
+        fs.resize(N);
+        std::vector<short> wave;
+        double f = 0;
+
+        for (const auto &px : pixelCol)
+        {
+            const HSV hsv = utils::RGBtoHSV(px.rgba);
+            f += freq_map(0, 360, _min_freq, _max_freq, hsv.h) /
+                 static_cast<double>(N);
+        }
+
+        wave = utils::sineWave(0.5, f, 1, _sample_rate);
+        fs   = utils::addVectors(fs, wave);
+        return fs;
+    }
+};
---
+++ include/sonify/DefaultPixelMappings/IntensityMap.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "sonify/MapTemplate.hpp"
+#include "sonify/utils.hpp"
+
+class IntensityMap : public MapTemplate
+{
+public:
+
+    const char *name() const override { return "Intensity"; }
+
+    std::vector<short>
+    mapping(const std::vector<Pixel> &pixelCol) noexcept override
+    {
+        const size_t N = pixelCol.size();
+        std::vector<short> fs(N);
+        double freq = 0;
+
+        if (N == 0) return {};
+
+        for (const auto &px : pixelCol)
+        {
+            const HSV hsv = utils::RGBtoHSV(px.rgba);
+            freq += freq_map(0, 1000, _min_freq, _max_freq, hsv.v);
+        }
+
+        utils::generateSineWave(fs, 0.25, freq, 0.05, _sample_rate);
+        utils::applyFadeInOut(fs);
+        utils::normalizeWave(fs);
+        return fs;
+    }
+};
