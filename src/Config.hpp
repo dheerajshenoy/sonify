058cf9e4829d85068b94f60a05a7db57b2313d43
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-08-26 23:49:34 +0530

    Add config struct

---
+++ src/Config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+struct Config
+{
+    int font_size{ 50 };
+    const std::string font_family{
+        "/usr/share/fonts/TTF/Comfortaa/static/Comfortaa-Bold.ttf"
+    };
+};
